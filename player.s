b692271d0485edfa1b7e31f8c93e8fc59fdfb32b
Levent Istifli <leventistifli@gmail.com>
2026-09-02 11:12:03 -0400

    Refactor player code

--- player.s
+++ player.s
@@ -31,30 +31,26 @@ player_init:
 .macro get_collision_at_point XOffset, YOffset
     stx scratch + 3
     sty scratch + 4
-    lda object_y_page_subpixels, x
-    and #PAGE_MASK
-    ora scratch + 5
-    sta object_y_page_subpixels, x
-    lda object_x_page_subpixels, x
+    lda object_x_page_subpixels, x ; Get page
     lsr
     lsr
     lsr
-    sta scratch + 10
-    lda object_x_positions, x
+    sta scratch + 10 ; Store in parameter location
+    lda object_x_positions, x ; Get X position
     .if XOffset <> 0
     clc
     adc #XOffset
     .endif
-    sta scratch + 11
-    lda scratch + 10
+    sta scratch + 11 ; Store in parameter location
+    lda scratch + 10 ; Update page if X position wrapped
     adc #$00
     sta scratch + 10
-    lda object_y_positions, x
+    lda object_y_positions, x ; Get Y Position
     .if YOffset <> 0
     clc
     adc #YOffset
     .endif
-    sta scratch + 12
+    sta scratch + 12 ; Store in parameter location
     jsr test_collision
 .endmacro
 
@@ -64,62 +60,61 @@ player_y_velocity = object_variables_1
 player_flags = object_variables_2
 PLAYER_IS_GROUNDED = %00000001
 PLAYER_ACCELERATION = 4 ; subpixels per frame
-PLAYER_MAX_SPEED = 24 ; subpixels per frame
+PLAYER_MAX_SPEED = 16 ; subpixels per frame
 PLAYER_JUMP_VELOCITY = 256 - 44 ; subpixels per frame
 PLAYER_WIDTH = 16
 player_step:
     lda object_animations_ids, x
     sta scratch + 2 ; Flag for new animation playing
     lda player_y_velocity, x ; Apply gravity
-    cmp #128
+    cmp #128 ; Check if velocity is negative
     bcc :+
-    adc #LIGHT_GRAVITY
+    adc #LIGHT_GRAVITY ; Use lighter gravity if going upwards
     jmp :++
     :
     clc
-    adc #GRAVITY
+    adc #GRAVITY ; Otherwise use regular gravity
     :
     cmp #128
     bcs :+
     cmp #TERMINAL_VELOCITY 
     bcc :+
-    lda #TERMINAL_VELOCITY
+    lda #TERMINAL_VELOCITY ; Cap downwards velocity
     :
     sta player_y_velocity, x
-    lda object_y_page_subpixels, x
+    lda object_y_page_subpixels, x ; Add Y velocity to position
     and #SUBPIXEL_MASK
     clc
     adc player_y_velocity, x
-    sta scratch + 5
-    cmp #SUBPIXEL_MASK
-    bcc :+
-    and #PAGE_MASK
+    sta scratch + 5 ; Store our new subpixel position for recombination later
+    cmp #SUBPIXEL_MASK ; Check if subpixel overflowed
+    bcc :+ ; Skip incrementing position if it didn't
+    and #PAGE_MASK ; Get whole part to add
     lsr
     lsr
     lsr
-    beq @after_add_y_velocity
     tay
-    lda player_flags, x
+    lda player_flags, x ; Since our position changed, reset the grounded flag
     and #$FF ^ PLAYER_IS_GROUNDED
     sta player_flags, x
     tya
-    cmp #%00010000
+    cmp #%00010000 ; Check if it's negative (offset by 3 bits because of the shifts)
     bcc @positive_y_velocity
-    ora #%11100000
+    ora #%11100000 ; Make negative before adding
     clc
     adc object_y_positions, x
     sta object_y_positions, x
-    bcs @after_add_y_velocity
+    bcs @after_add_y_velocity ; Check if the Y position crossed a page (but there's no vertical scrolling, so this is actually unnecessary)
     lda object_y_page_subpixels, x
     and #PAGE_MASK
     bne :+
-    lda #$00
+    lda #$00 ; Reset the page to 0 if it underflowed
     sta object_y_positions, x
     sta object_y_page_subpixels, x
     sta player_y_velocity, x
     jmp @after_add_y_velocity
     :
-    sec
+    sec ; Decrement the page
     sbc #SUBPIXEL_MASK + 1
     sta object_y_page_subpixels, x
     jmp @after_add_y_velocity
@@ -128,33 +123,40 @@ player_step:
     adc object_y_positions, x
     sta object_y_positions, x
     bcc :+
-    lda object_y_page_subpixels, x
+    lda object_y_page_subpixels, x ; Increment page
     adc #SUBPIXEL_MASK
     sta object_y_page_subpixels, x
     :
     @after_add_y_velocity:
-    ldy #$08
+    lda scratch + 5 ; Apply subpixels we stored earlier
+    and #SUBPIXEL_MASK
+    sta scratch + 5
+    lda object_y_page_subpixels, x
+    and #PAGE_MASK
+    ora scratch + 5
+    sta object_y_page_subpixels, x
+    ldy #$08 ; Limit collision checks to 8 so that we don't infinitely loop
     lda player_y_velocity, x
     bne :+
-    jmp @after_vertical_collision
+    jmp @after_vertical_collision ; Don't check vertical collision if Y velocity is 0
     :
-    bpl @test_floor_collision_left
+    bpl @test_floor_collision_left ; Don't check ceiling collision if Y velocity is positive
     jmp @test_ceiling_collision_left
     @test_floor_collision_left:
     get_collision_at_point 5, 15
     ldx scratch + 3
     and #COLLISION_TOP
-    beq @after_test_floor_collision_left
-    lda object_y_page_subpixels, x
+    beq @after_test_floor_collision_left ; Ignore this tile if it's not solid on top
+    lda object_y_page_subpixels, x ; Set subpixel back to 0
     and #PAGE_MASK
     sta object_y_page_subpixels, x
     lda object_y_positions, x
     sec
-    sbc #$01
+    sbc #$01 ; Snap position up
     and #$F0
     sta object_y_positions, x
     lda #$00
-    sta player_y_velocity, x
+    sta player_y_velocity, x ; Reset Y velocity
     lda player_flags, x
     ora #PLAYER_IS_GROUNDED
     sta player_flags, x ; Store that we're on the ground
@@ -228,9 +230,9 @@ player_step:
     @after_test_ceiling_collision_right:
     @after_vertical_collision:
     lda controller_input
-    and #BUTTON_RIGHT + BUTTON_LEFT
+    and #BUTTON_RIGHT + BUTTON_LEFT ; Check if we're pressing left or right
     beq @not_pressing_anything
-    cmp #BUTTON_RIGHT + BUTTON_LEFT
+    cmp #BUTTON_RIGHT + BUTTON_LEFT ; Ignore horizontal inputs if pressing left and right
     beq @not_pressing_anything
     and #BUTTON_RIGHT
     beq @pressing_left
@@ -238,7 +240,7 @@ player_step:
     lda player_flags, x
     and #PLAYER_IS_GROUNDED
     beq :+
-    lda #ANIM_PLAYER_WALK
+    lda #ANIM_PLAYER_WALK ; If we're on the ground and pressing right, play the walk animation
     sta scratch + 2
     :
     lda player_x_velocity, x
@@ -246,7 +248,7 @@ player_step:
     adc #PLAYER_ACCELERATION
     cmp #128
     bcs :+
-    cmp #PLAYER_MAX_SPEED
+    cmp #PLAYER_MAX_SPEED ; Cap horizontal speed
     bcc :+
     lda #PLAYER_MAX_SPEED
     :
@@ -272,13 +274,13 @@ player_step:
     lda player_flags, x
     and #PLAYER_IS_GROUNDED
     beq :+
-    lda #ANIM_PLAYER_IDLE
+    lda #ANIM_PLAYER_IDLE ; Play idle animation if on the ground
     sta scratch + 2
     :
-    lda player_x_velocity, x
+    lda player_x_velocity, x ; Move velocity towards 0
     cmp #%10000000
     bcc @decrease_x_velocity
-    lda player_x_velocity, x
+    lda player_x_velocity, x ; If velocity is negative
     clc
     adc #PLAYER_ACCELERATION
     bcc :+
@@ -286,7 +288,7 @@ player_step:
     :
     jmp @after_determine_x_velocity
     @decrease_x_velocity:
-    lda player_x_velocity, x
+    lda player_x_velocity, x ; If velocity is positive
     sec
     sbc #PLAYER_ACCELERATION
     bcs :+
@@ -294,17 +296,17 @@ player_step:
     :
     @after_determine_x_velocity:
     sta player_x_velocity, x
-    lda player_x_velocity, x
-    beq :++
+    tay ; Reset zero flag
+    beq :++ ; Don't change the direction if the velocity is 0
     cmp #%10000000
     bcc :+
     lda object_flags, x
-    ora #OBJECT_FLIPPED_H
+    ora #OBJECT_FLIPPED_H ; Flip if facing to the left
     sta object_flags, x
     jmp :++
     :
     lda object_flags, x
-    and #255 - OBJECT_FLIPPED_H
+    and #$FF ^ OBJECT_FLIPPED_H ; Unflip if facing to the right
     sta object_flags, x
     :
     lda object_x_page_subpixels, x
@@ -462,14 +464,11 @@ player_step:
     sta player_y_velocity, x
     @after_release_jump:
     @after_air_code:
-    lda scratch + 2
+    lda scratch + 2 ; Load animation to play
     cmp object_animations_ids, x
     beq :+
     tay
     sta object_animations_ids, x
-    jsr init_animation
-    jmp :++
+    jmp init_animation ; If it's different from our current animation, initialize it
     :
-    jsr play_animation
-    :
-    rts
+    jmp play_animation ; Otherwise continue to play current animation
